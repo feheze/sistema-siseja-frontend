8c7c84c8af0b5b3606bdf49c9c858ca4d69938ae
feheze@gmail.com <feheze@gmail.com>
2025-10-05 00:06:21 -0300

    fix(ci): corrigir render mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Server
-  }
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'polos/:id', // Rota para detalhes do polo
+    renderMode: RenderMode.Server // Renderiza no servidor
+  },
 ];
